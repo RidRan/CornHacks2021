c3191aea6c9c1a3a37ac1c7754579e566ae79358
RidRan <riddock.moran@huskers.unl.edu>
2021-01-30 18:08:15 -0600

    Added street and water as well as boat controls

---
+++ Cornhacks/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+
+    float horizontal, vertical;
+    public float horizontalSpeed = 3f, verticalSpeed = 3f;
+
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        transform.position = rigidbody2d.position;
+    }
+
+    void FixedUpdate() {
+
+        float frameTime = Time.deltaTime;
+
+        Vector2 position = rigidbody2d.position;
+        position.x += horizontalSpeed * horizontal * frameTime;
+        position.y += verticalSpeed * vertical * frameTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+}
