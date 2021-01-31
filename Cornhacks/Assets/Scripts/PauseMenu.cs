a14f55b94b688b6428cdfe4691fc6a083cfe5dc1
RidRan <riddock.moran@huskers.unl.edu>
2021-01-31 02:43:30 -0600

    Added drain and pause menu

---
+++ Cornhacks/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool paused = false;
+
+    public GameObject menu;
+
+    public Player player;
+
+    void Start() {
+        menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (paused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() {
+        paused = false;
+        menu.SetActive(false);
+        player.menu = false;
+    }
+
+    void Pause() {
+        paused = true;
+        menu.SetActive(true);
+        player.menu = true;
+    }
+
+    public void Quit() {
+        Debug.Log("Qutting");
+        Application.Quit();
+    }
+}
--- Cornhacks/Assets/Scripts/Player.cs
+++ Cornhacks/Assets/Scripts/Player.cs
@@ -8,8 +8,12 @@ public class Player : MonoBehaviour
 
     Animator animator;
 
+    public bool menu = true, rippleOn = false;
+
+    public ParticleSystem ripple;
+
     float horizontal, vertical;
-    public float horizontalSpeed = 3f, verticalSpeed = 3f;
+    public float horizontalSpeed = 3f, verticalSpeed = 3f, verticalMultiplier = 3f;
 
     public float timeInvincible = 2.0f;
     bool isInvincible;
@@ -24,39 +28,54 @@ public class Player : MonoBehaviour
         animator = GetComponent<Animator>();
 
         rigidbody2d = GetComponent<Rigidbody2D>();
+        ripple.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        if (!menu) {
+            if (!rippleOn) {
+                ripple.Play();
+                rippleOn = true;
+            }
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
 
-        if (horizontal < 0) {
-            left = true;
-            right = false;
-        } else if (horizontal > 0) {
-            left = false;
-            right = true;
-        } else {
-            left = false;
-            right = false;
+            if (horizontal < 0) {
+                left = true;
+                right = false;
+            } else if (horizontal > 0) {
+                left = false;
+                right = true;
+            } else {
+                left = false;
+                right = false;
+            }
+
+            animator.SetBool("Turning Left", left);
+            animator.SetBool("Turning Right", right);
+
+            transform.position = rigidbody2d.position;
+        } else if (rippleOn) {
+            ripple.Stop();
+            rippleOn = false;
         }
 
-        animator.SetBool("Turning Left", left);
-        animator.SetBool("Turning Right", right);
-
-        transform.position = rigidbody2d.position;
+        
+        
     }
 
     void FixedUpdate() {
+        if (!menu) {
+            float frameTime = Time.deltaTime;
 
-        float frameTime = Time.deltaTime;
+            Vector2 position = rigidbody2d.position;
+            position.x += horizontalSpeed * horizontal * frameTime;
+            position.y += (verticalSpeed + vertical * verticalMultiplier) * frameTime;
 
-        Vector2 position = rigidbody2d.position;
-        position.x += horizontalSpeed * horizontal * frameTime;
-        position.y += verticalSpeed * vertical * frameTime;
-
-        rigidbody2d.MovePosition(position);
+            rigidbody2d.MovePosition(position);
+        }
+        
     }
 }
